5a8134bf775e8fcdb5791e10c461633dbfc6934b
SultanSarygulov <itsme.kaan.sarygulov@gmail.com>
2022-11-25 02:37:51 +0600

    WebView and Progress Bar added

--- app/src/main/java/com/example/newsapplication/presentation/ArticleFragment.kt
+++ app/src/main/java/com/example/newsapplication/presentation/ArticleFragment.kt
@@ -1,10 +1,16 @@
 package com.example.newsapplication.presentation
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import androidx.annotation.RequiresApi
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.example.newsapplication.R
@@ -15,24 +21,46 @@ class ArticleFragment : Fragment() {
     private lateinit var binding: FragmentArticleBinding
     private val args by navArgs<ArticleFragmentArgs>()
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentArticleBinding.inflate(inflater, container, false)
-
-        binding.apply {
-            articleTitleRead.setText(args.currentArticle.title)
-            articleDescriptionRead.setText(args.currentArticle.description)
-            articleContentRead.setText(args.currentArticle.content)
-            Glide
-                .with(root)
-                .load(args.currentArticle.urlToImage)
-                .into(articleImageRead)
-        }
-
+//        binding.apply {
+//            articleTitleRead.setText(args.currentArticle.title)
+//            articleDescriptionRead.setText(args.currentArticle.description)
+//            articleContentRead.setText(args.currentArticle.content)
+//            Glide
+//                .with(root)
+//                .load(args.currentArticle.urlToImage)
+//                .into(articleImageRead)
+//        }
+        Log.d("TAG", "Started")
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.webview.webViewClient = WebViewClient()
+        binding.webview.loadUrl(args.currentArticle.url)
+    }
+
+    inner class WebViewClient: android.webkit.WebViewClient(){
+
+        override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+            view.loadUrl(url)
+            return false
+        }
+
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
+            Log.d("TAG", "Finished")
+            binding.progressBar.isVisible = false
+            binding.webview.isVisible = true
+        }
+    }
 }
